f49567bfa9382598db2137989db216acc2ed626b
twekberg <tekberg@gmail.com>
2024-02-06 07:33:38 -0800

    Split directory into 2 parts.

--- dups.py
+++ dups.py
@@ -23,14 +23,23 @@ def build_parser():
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-i', '--input_directory',
-                        default='F:/N/O/SPELLSNO/IMAGES/xxxbunker/EDITED/BELOW',
+    parser.add_argument('-i', '--input_directory_base',
+                        default='F:/N/O/SPELLSNO/IMAGES/xxxbunker/EDITED',
                         help='The name of the directory containing'
                         ' video clips.'
                         ' Default: %(default)s.')
-    parser.add_argument('-o', '--output_directory',
-                        default='F:/N/O/SPELLSNO/IMAGES/xxxbunker/EDITED/CLIPS/BELOW',
+    parser.add_argument('-s', '--sub_directory',
+                        default='BELOW',
+                        help='Subdirectory under input_directory_base containing'
+                        ' video clips.'
+                        ' Default: %(default)s.')
+    parser.add_argument('-o', '--output_directory_base',
+                        default='F:/N/O/SPELLSNO/IMAGES/xxxbunker/EDITED/CLIPS',
                         help='Input directory. '
+                        ' Default: %(default)s.')
+    parser.add_argument('-f', '--frame', type=int,
+                        default=5,
+                        help='Frame number to use as thumbnail. '
                         ' Default: %(default)s.')
     return parser
 
@@ -39,8 +48,6 @@ def exec_one(prog, args):
     """
     Run program with args. Returns stdout, throw exception when status != 0.
     """
-    #result = subprocess.Popen(["./ffprobe", filename],
-    print([prog] + args)
     result = subprocess.run([prog] + args,
                             stdout = subprocess.PIPE,
                             text=True,
@@ -49,29 +56,34 @@ def exec_one(prog, args):
 
 
 def path_dos2unix(path):
+    """
+    Convert a Path object to Unix format as a string.
+    """
     return str(path).replace('\\', '/')
+
 
 def main(args):
     """
     Starting point.
     """
-    output_directory = Path(args.output_directory)
+    output_directory = Path(args.output_directory_base) / args.sub_directory
     if not output_directory.is_dir():
         if output_directory.exists():
             print('Output directory already exists but is not a directory. {output_directory=}')
             exit(1)
         output_directory.mkdir(mode=0o755, parents=True, exist_ok=True)
-    input_directory = Path(args.input_directory)
+    input_directory = Path(args.input_directory_base) / args.sub_directory
     if not output_directory.is_dir():
         print(f"Input directory already doesn't exist. {input_directory=}")
         exit(1)
 
     for file in input_directory.glob('*.mpg'):
-        print(file)
-        clip_filename = path_dos2unix(output_directory / (file.stem + '.jpg'))
-        stdout = exec_one('magick', ['convert', f'{path_dos2unix(file)}[5]', str(clip_filename)])
-        print(f'{stdout}')
-        exit(0)
+        clip_filename = output_directory / (file.stem + '.jpg')
+        if clip_filename.exists():
+            # The clip already exists.
+            continue
+        stdout = exec_one('magick', ['convert', f'{path_dos2unix(file)}[{args.frame}]',
+                                     path_dos2unix(clip_filename)])
 
 
 if __name__ == '__main__':
